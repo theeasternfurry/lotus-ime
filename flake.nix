e21f6d48c9ea9300cdafe6fc68f45b3476e8a631
theeasternfurry <theeasternfurry@outlook.com>
2025-08-21 12:06:59 +0700

    Add nix flake devShell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  with pkgs;
+  {
+    devShells.${system}.default = mkShell {
+      buildInputs = [
+        cargo
+        rustc
+      ];
+    };
+  };
+}
