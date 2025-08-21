b87c3bc874da8f39eaae274cbd7d9a037e59b1cc
theeasternfurry <theeasternfurry@outlook.com>
2025-08-21 12:49:13 +0700

    Create Input struct and test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
+use lotus_ime::input::{Input};
+
 fn main() {
     let inputs = vec![
         "viet65",
         "nam"
     ];
 
-    let mut result = String::new();
-    for input in inputs {
-        vi::transform_buffer(&vi::VNI, input.chars(), &mut result);
-        result.push(' ');
-    }
+    let config = Input::new();
 
-    println!("{}", result); // prints "việt nam "
+    println!("{}", config.return_text()); // prints "việt nam "
 }
